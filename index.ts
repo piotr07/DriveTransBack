8da769dacd16088ba16390d91e2a46986a4f634a
Piotr Rodak <piotrrodak1@gmail.com>
2022-07-12 22:46:21 +0200

    Init commit.

---
+++ index.ts
@@ -0,0 +1,25 @@
+import express, {json} from "express";
+import cors from 'cors';
+import 'express-async-errors';
+// import {handleError} from "./utils/error";
+// import rateLimit from 'express-rate-limit';
+// import {adRouter} from "./routers/ad.router";
+
+const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+}));
+app.use(json());
+// app.use(rateLimit({
+//     windowMs: 5 * 60 * 1000, // 15 minutes
+//     max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+// }));
+//
+// app.use('/ad', adRouter)
+//
+// app.use(handleError);
+
+app.listen(3001, '0.0.0.0', () => {
+    console.log('Listening on port http://localhost:3001');
+})
